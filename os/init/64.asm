127b97d5d2c8c26b331edbf354dbb4873e363fd1
Shark <shark@krellmachine>
2014-02-05 16:13:03 +0100

    varius optimizations

--- os/init/64.asm
+++ os/init/64.asm
@@ -12,42 +12,35 @@ align 16
 
 init_64:
 	; Make sure that memory range 0x110000 - 0x200000 is cleared
-	mov rdi, os_SystemVariables
-	mov rcx, 122880            ; Clear 960 KiB
-	xor rax, rax
+	mov edi, os_SystemVariables
+	mov ecx, 122880            ; Clear 960 KiB
+	xor eax, eax
 	rep stosq                  ; Store rax to [rdi], rcx - 1, rdi + 8, if rcx > 0 then do it again
 
 	mov word [os_Screen_Rows], 25
 	mov word [os_Screen_Cols], 80
-	mov rsi, 0x5080
-	lodsd
-	cmp eax, 0
+	cmp eax, [0x5080]
 	je nographics
 	call init_screen
 nographics:
-
-	mov word [os_Screen_Cursor_Row], 0
-	mov word [os_Screen_Cursor_Col], 0
+	xor eax, eax
+	mov [os_Screen_Cursor_Row], eax
 	call os_screen_clear		; Clear screen and display cursor
 
 	; Display CPU information
-	mov ax, [os_Screen_Rows]
-	sub ax, 5
-	mov word [os_Screen_Cursor_Row], ax
-	mov word [os_Screen_Cursor_Col], 0
+	movzx eax, [os_Screen_Rows]
+	sub eax, 5
+	mov dword [os_Screen_Cursor_Row], eax
 	mov rsi, cpumsg
 	call os_output
-	xor eax, eax
-	mov rsi, 0x5012
-	lodsw
+	movzx eax, word [0x5012]
 	mov rdi, os_temp_string
 	mov rsi, rdi
 	call os_int_to_string
 	call os_output
 	mov rsi, coresmsg
 	call os_output
-	mov rsi, 0x5010
-	lodsw
+	movzx eax, word [0x5010]
 	mov rdi, os_temp_string
 	mov rsi, rdi
 	call os_int_to_string
@@ -55,15 +48,16 @@ nographics:
 	mov rsi, mhzmsg
 	call os_output
 
-	xor rdi, rdi 			; Create the 64-bit IDT (at linear address 0x0000000000000000) as defined by Pure64
+	xor edi, edi 			; Create the 64-bit IDT (at linear address 0x0000000000000000) as defined by Pure64
 
 	; Create exception gate stubs (Pure64 has already set the correct gate markers)
-	mov rcx, 32
+	xor ecx, ecx
+	mov cl, 32
 	mov rax, exception_gate
 make_exception_gate_stubs:
 	call create_gate
-	add rdi, 1
-	sub rcx, 1
+	inc rdi
+	dec rcx
 	jnz make_exception_gate_stubs
 
 	; Create interrupt gate stubs (Pure64 has already set the correct gate markers)
@@ -71,35 +65,36 @@ make_exception_gate_stubs:
 	mov rax, interrupt_gate
 make_interrupt_gate_stubs:
 	call create_gate
-	add rdi, 1
-	sub rcx, 1
+	inc rdi
+	dec rcx
 	jnz make_interrupt_gate_stubs
 
 	; Set up the exception gates for all of the CPU exceptions
-	mov rcx, 20
+	xor ecx, ecx
+	mov cl, 20
 	xor rdi, rdi
 	mov rax, exception_gate_00
 make_exception_gates:
 	call create_gate
-	add rdi, 1
-	add rax, 16			; The exception gates are aligned at 16 bytes
-	sub rcx, 1
+	inc edi	
+	add eax, 16			; The exception gates are aligned at 16 bytes
+	dec rcx 
 	jnz make_exception_gates
 
 	; Set up the IRQ handlers (Network IRQ handler is configured in init_net)
-	mov rdi, 0x21
+	mov edi, 0x21
 	mov rax, keyboard
 	call create_gate
-	mov rdi, 0x22
+	add edi, 1
 	mov rax, cascade
 	call create_gate
-	mov rdi, 0x28
+	add edi, 6
 	mov rax, rtc
 	call create_gate
-	mov rdi, 0x80
+	mov edi, 0x80
 	mov rax, ap_wakeup
 	call create_gate
-	mov rdi, 0x81
+	add edi, 1
 	mov rax, ap_reset
 	call create_gate
 
@@ -109,6 +104,7 @@ make_exception_gates:
 	; RTC value must stay at 32.768KHz or the computer will not keep the correct time
 	; http://wiki.osdev.org/RTC
 rtc_poll:
+	xor eax, eax
 	mov al, 0x0A			; Status Register A
 	out 0x70, al
 	in al, 0x71
@@ -121,11 +117,11 @@ rtc_poll:
 	mov al, 0x0B			; Status Register B
 	out 0x70, al			; Select the address
 	in al, 0x71			; Read the current settings
-	push rax
+	movzx ebx, al
 	mov al, 0x0B			; Status Register B
 	out 0x70, al			; Select the address
-	pop rax
-	bts ax, 6			; Set Periodic(6)
+	bts ebx, 6			; Set Periodic(6)
+	mov eax, ebx
 	out 0x71, al			; Write the new settings
 	mov al, 0x0C			; Acknowledge the RTC
 	out 0x70, al
@@ -136,79 +132,79 @@ rtc_poll:
 	mov dx, 0x03C8			; DAC Address Write Mode Register
 	out dx, al
 	mov dx, 0x03C9			; DAC Data Register
-	mov rbx, 16			; 16 lines
+	xor ebx, ebx
+	xor ecx, ecx
+	mov bl, 16			; 16 lines
 nextline:
-	mov rcx, 16			; 16 colors
+	mov cl, 16			; 16 colors
 	mov rsi, palette
 nexttritone:
-	lodsb
+	mov eax, [rsi]
 	out dx, al
-	lodsb
+	shr eax, 8
 	out dx, al
-	lodsb
+	shr eax, 8
 	out dx, al
-	dec rcx
-	cmp rcx, 0
-	jne nexttritone
-	dec rbx
-	cmp rbx, 0
-	jne nextline			; Set the next 16 colors to the same
-	mov eax, 0x14			; Fix for color 6
-	mov dx, 0x03c8			; DAC Address Write Mode Register
+	add rsi, 3
+	dec ecx
+	jnz nexttritone
+	dec ebx
+	jnz nextline			; Set the next 16 colors to the same
+	xor eax, eax
+	mov al, 0x14			; Fix for color 6
+	mov edx, 0x03c8			; DAC Address Write Mode Register
 	out dx, al
 	mov dx, 0x03c9			; DAC Data Register
 	mov rsi, palette
 	add rsi, 18
-	lodsb
+	mov eax, [rsi+18]
 	out dx, al
-	lodsb
+	shl eax, 8
 	out dx, al
-	lodsb
+	shl eax, 8
 	out dx, al
 
 	xor eax, eax
 	xor ebx, ebx
 	xor ecx, ecx
 	; Grab data from Pure64's infomap
-	mov rsi, 0x5008
-	lodsd				; Load the BSP ID
-	mov ebx, eax			; Save it to EBX
+	mov ebx, [0x5008]		; Load the BSP ID
+					; Save it to EBX
 	mov rsi, 0x5012
-	lodsw				; Load the number of activated cores
-	mov cx, ax			; Save it to CX
-	mov rsi, 0x5060
-	lodsq
+	mov eax, [0x5012]		; Load the number of activated cores
+	movzx ecx, ax			; Save it to CX
+	mov esi, 0x5060
+	mov rax, [rsi]
 	mov [os_LocalAPICAddress], rax
-	lodsq
+	mov rax, [rsi+8]
 	mov [os_IOAPICAddress], rax
 
-	mov rsi, 0x5012
-	lodsw
+;	mov rsi, 0x5012
+	mov eax, [rsi-0x4e]
 	mov [os_NumCores], ax
 
 	mov rsi, 0x5020
-	lodsd
+	mov eax, [0x5020]
 	mov [os_MemAmount], eax		; In MiB's
 
-	mov rsi, 0x5040
-	lodsq
+	mov rax, [0x5040]
 	mov [os_HPETAddress], rax
 
 	; Build the OS memory table
 	call init_memory_map
 
 	; Initialize all AP's to run our reset code. Skip the BSP
-	xor rax, rax
-	mov rsi, 0x0000000000005100	; Location in memory of the Pure64 CPU data
+	xor eax, eax
+	mov esi, 0x5100	; Location in memory of the Pure64 CPU data
 next_ap:
-	cmp cx, 0
-	je no_more_aps
+	test ecx, ecx
+	jz no_more_aps
 	lodsb				; Load the CPU APIC ID
 	cmp al, bl
 	je skip_ap
 	call os_smp_reset		; Reset the CPU
 skip_ap:
-	sub cx, 1
+	dec ecx
 	jmp next_ap
 
 no_more_aps:
@@ -225,6 +221,7 @@ no_more_aps:
 	call os_output
 
 	; Enable specific interrupts
+	xor eax, eax
 	in al, 0x21
 	mov al, 11111001b		; Enable Cascade, Keyboard
 	out 0x21, al
@@ -238,19 +235,18 @@ no_more_aps:
 ; rax = address of handler
 ; rdi = gate # to configure
 create_gate:
-	push rdi
-	push rax
+	mov r8, rdi
+	mov r9, rax
 
-	shl rdi, 4			; quickly multiply rdi by 16
-	stosw				; store the low word (15..0)
+	shl r8, 4			; quickly multiply rdi by 16
+	mov [r8], ax			; store the low word (15..0)
 	shr rax, 16
-	add rdi, 4			; skip the gate marker
-	stosw				; store the high word (31..16)
+					; skip the gate marker
+	mov [r8+6], ax			; store the high word (31..16)
 	shr rax, 16
-	stosd				; store the high dword (63..32)
+	mov [r8+8], eax			; store the high dword (63..32)
 
-	pop rax
-	pop rdi
+	mov rax, r9
 	ret
 
 
@@ -261,24 +257,22 @@ init_memory_map:			; Build the OS memory table
 
 	; Build a fresh memory map for the system
 	mov rdi, os_MemoryMap
-	push rdi
-	xor rcx, rcx
-	mov cx, [os_MemAmount]
-	shr cx, 1			; Divide actual memory by 2
+	movzx ecx, word [os_MemAmount]
+	shr ecx, 1			; Divide actual memory by 2
+	xor eax, eax
 	mov al, 1
 	rep stosb
-	pop rdi
-	mov al, 2
-	stosb				; Mark the first 2 MiB as in use (by Kernel and system buffers)
+	mov rdi, os_MemoryMap
+	inc eax
+	mov [rdi], al			; Mark the first 2 MiB as in use (by Kernel and system buffers)
 ;	stosb				; As well as the second 2 MiB (by loaded application)
 	; The CLI should take care of the Application memory
 
 	; Allocate memory for CPU stacks (2 MiB's for each core)
-	xor rcx, rcx
-	mov cx, [os_NumCores]		; Get the amount of cores in the system
+	movzx ecx, word [os_NumCores]	; Get the amount of cores in the system
 	call os_mem_allocate		; Allocate a page for each core
-	cmp rcx, 0			; os_mem_allocate returns 0 on failure
-	je system_failure
+	test rcx, rcx			; os_mem_allocate returns 0 on failure
+	jz system_failure
 	add rax, 2097152
 	mov [os_StackBase], rax		; Store the Stack base address
 
@@ -299,59 +293,50 @@ system_failure_hang:
 
 ; -----------------------------------------------------------------------------
 init_screen:
-	mov rsi, 0x5080
-	xor eax, eax
-	lodsd				; VIDEO_BASE
-	mov [os_VideoBase], rax
-	xor eax, eax
-	xor ecx, ecx
+	mov rax, [0x5080]		; VIDEO_BASE
+	mov esi, 0x5080
+	mov ecx, eax			; 0 extend to 64bit
+	mov [os_VideoBase], rcx
 
-	lodsw				; VIDEO_X
+	shr rax, 32			; VIDEO_X
 	mov [os_VideoX], ax		; ex: 1024
-
+	movzx r8d, ax
+	
 	xor edx, edx
-	mov cl, [font_width]
+	movzx ecx, byte [font_width]
 	div cx
 	mov [os_Screen_Cols], ax
 
-	lodsw				; VIDEO_Y
+	mov eax, [rsi+8]		; VIDEO_Y
 	mov [os_VideoY], ax		; ex: 768
-
+	movzx r9d, ax
 	xor edx, edx
-	mov cl, [font_height]
+	movzx ecx, byte [font_height]
+	mov r10d, ecx			; Save font_height
 	div cx
 	mov [os_Screen_Rows], ax
 
-	lodsb				; VIDEO_DEPTH
+	shr eax, 24				; VIDEO_DEPTH
 	mov [os_VideoDepth], al
+	mov r11d, r8d				; Save VIDEO_X
+	imul r8d, r9d
+	mov [os_Screen_Pixels], r8d
+	movzx ecx, byte [os_VideoDepth]
+	shr ecx, 3
+	mov r12d, ecx				; Save VideoDepth
+	imul ecx, r8d
+	mov [os_Screen_Bytes], ecx
 
-	xor eax, eax
+	imul r10d, r11d
+	mov ecx, r12d
+	imul ecx, r10d
+	mov dword [os_Screen_Row_2], ecx
 	xor ecx, ecx
-	mov ax, [os_VideoX]
-	mov cx, [os_VideoY]
-	mul ecx
-	mov [os_Screen_Pixels], eax
-	xor ecx, ecx
-	mov cl, [os_VideoDepth]
-	shr cl, 3
-	mul ecx
-	mov [os_Screen_Bytes], eax
-
-	xor eax, eax
-	xor ecx, ecx
-	mov ax, [os_VideoX]
-	mov cl, [font_height]
-	mul cx
-	mov cl, [os_VideoDepth]
-	shr cl, 3
-	mul ecx
-	mov dword [os_Screen_Row_2], eax
-
+	mov cl, 1
 	mov eax, 0x00FFFFFF
 	mov [os_Font_Color], eax
 
-	mov al, 1
-	mov [os_VideoEnabled], al
+	mov [os_VideoEnabled], cl
 
 	ret
 ; -----------------------------------------------------------------------------
