76c1335f3472dafa5d7657893f2297b37f657ca0
Shark <shark@krellmachine>
2014-02-06 20:17:11 +0100

    use cmpxchg and pause for locks, inline calls

--- os/syscalls/smp.asm
+++ os/syscalls/smp.asm
@@ -17,18 +17,16 @@ align 16
 ; Note:	This code resets an AP
 ;	For setup use only.
 os_smp_reset:
-	push rdi
-	push rax
+	mov r10, rax
 
-	mov rdi, [os_LocalAPICAddress]
+	mov r8 [os_LocalAPICAddress]
 	shl eax, 24		; AL holds the CPU #, shift left 24 bits to get it into 31:24, 23:0 are reserved
-	mov [rdi+0x0310], eax	; Write to the high bits first
+	mov [r8+0x0310], eax	; Write to the high bits first
 	xor eax, eax		; Clear EAX, namely bits 31:24
 	mov al, 0x81		; Execute interrupt 0x81
-	mov [rdi+0x0300], eax	; Then write to the low bits
+	mov [r8+0x0300], eax	; Then write to the low bits
 
-	pop rax
-	pop rdi
+	mov rax, r10
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -38,18 +36,16 @@ os_smp_reset:
 ;  IN:	AL = CPU #
 ; OUT:	Nothing. All registers preserved.
 os_smp_wakeup:
-	push rdi
-	push rax
+	mov r10, rax
 
-	mov rdi, [os_LocalAPICAddress]
+	mov r8, [os_LocalAPICAddress]
 	shl eax, 24		; AL holds the CPU #, shift left 24 bits to get it into 31:24, 23:0 are reserved
-	mov [rdi+0x0310], eax	; Write to the high bits first
+	mov [r8+0x0310], eax	; Write to the high bits first
 	xor eax, eax		; Clear EAX, namely bits 31:24
 	mov al, 0x80		; Execute interrupt 0x80
-	mov [rdi+0x0300], eax	; Then write to the low bits
+	mov [r8+0x0300], eax	; Then write to the low bits
 
-	pop rax
-	pop rdi
+	mov rax, r10
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -59,17 +55,14 @@ os_smp_wakeup:
 ;  IN:	Nothing.
 ; OUT:	Nothing. All registers preserved.
 os_smp_wakeup_all:
-	push rdi
-	push rax
-
-	mov rdi, [os_LocalAPICAddress]
+	mov r9, rax
+	mov r8, [os_LocalAPICAddress]
 	xor eax, eax
-	mov [rdi+0x0310], eax	; Write to the high bits first
+	mov [r8+0x0310], eax	; Write to the high bits first
 	mov eax, 0x000C0080	; Execute interrupt 0x80
-	mov [rdi+0x0300], eax	; Then write to the low bits
+	mov [r8+0x0300], eax	; Then write to the low bits
 
-	pop rax
-	pop rdi
+	mov rax, r9
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -79,15 +72,10 @@ os_smp_wakeup_all:
 ;  IN:	Nothing
 ; OUT:	RAX = CPU's APIC ID number, All other registers perserved.
 os_smp_get_id:
-	push rsi
 
-	xor eax, eax
-	mov rsi, [os_LocalAPICAddress]
-	add rsi, 0x20		; Add the offset for the APIC ID location
-	lodsd			; APIC ID is stored in bits 31:24
-	shr rax, 24		; AL now holds the CPU's APIC ID (0 - 255)
+	mov r9, [os_LocalAPICAddress]
+	mov eax, [r9+0x23]	; APIC ID is stored in bits 31:24
 
-	pop rsi
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -98,54 +86,71 @@ os_smp_get_id:
 ;	RSI = Variable
 ; OUT:	Nothing
 os_smp_enqueue:
-	push rdi
-	push rsi
-	push rcx
-	push rax
+	mov  r9, rdi
+	mov r10, rsi
+	mov r11, rcx
+	mov r12, rbx
+	mov r15, rax
 
 os_smp_enqueue_spin:
-	bt word [os_QueueLock], 0	; Check if the mutex is free
-	jc os_smp_enqueue_spin		; If not check it again
-	lock bts word [os_QueueLock], 0	; The mutex was free, lock the bus. Try to grab the mutex
-	jc os_smp_enqueue_spin		; Jump if we were unsuccessful
-
-	cmp word [os_QueueLen], 256	; aka cpuqueuemax
-	je os_smp_enqueue_fail
+;	bt word [os_QueueLock], 0	; Check if the mutex is free
+;	jc os_smp_enqueue_spin		; If not check it again
+;	lock bts word [os_QueueLock], 0	; The mutex was free, lock the bus. Try to grab the mutex
+;	jc os_smp_enqueue_spin		; Jump if we were unsuccessful
 
 	xor ecx, ecx
+	mov cl, 1
+	xor eax, eax
+	lock cmpxchg cl, [os_QueueLock]
+	jz mutex_free
+	pause
+	jmp os_smp_dequeue_spin
+
+mutex_free:
+	movzx edx, word [os_QueueLen]	; aka cpuqueuemax
 	mov rdi, cpuqueue
-	mov cx, [cpuqueuefinish]
-	shl rcx, 4			; Quickly multiply RCX by 16
+	movzx ecx, word [cpuqueuefinish]
+	mov eax, ecx
+	shl ecx, 4			; Quickly multiply RCX by 16
 	add rdi, rcx
+	cmp edx, 256
+	je os_smp_enqueue_fail
 
-	stosq				; Store the code address from RAX
-	mov rax, rsi
-	stosq				; Store the variable
 
-	add word [os_QueueLen], 1
-	shr rcx, 4			; Quickly divide RCX by 16
-	add cx, 1
-	cmp cx, [cpuqueuemax]
-	jne os_smp_enqueue_end
-	xor cx, cx			; We wrap around
+	mov [rdi], r15				; Store the code address from RAX
+	mov [rdi+8], rsi			; Store the variable
+
+	add edx 1
+	inc eax
+	xor ebx, ebx
+	mov [os_QueueLen], dx
+	cmp ax, [cpuqueuemax]
+	cmove ecx, ebx
 
 os_smp_enqueue_end:
-	mov [cpuqueuefinish], cx
-	pop rax
-	pop rcx
-	pop rsi
-	pop rdi
-	btr word [os_QueueLock], 0	; Release the lock
-	call os_smp_wakeup_all
+	mov [cpuqueuefinish], ax
+	mov [os_QueueLock], bx		; Release the lock
+	mov r8, [os_LocalAPICAddress]
+	mov rbx, r12
+	mov rcx, r11
+	mov rsi, r10
+	mov rdi, r9
+	xor eax, eax
+	mov [r8+0x0310], eax	; Write to the high bits first
+	mov eax, 0x000C0080	; Execute interrupt 0x80
+	mov [r8+0x0300], eax	; Then write to the low bits
+	mov rax, r15
 	clc				; Carry clear for success
 	ret
 
 os_smp_enqueue_fail:
-	pop rax
-	pop rcx
-	pop rsi
-	pop rdi
-	btr word [os_QueueLock], 0	; Release the lock
+	xor eax, eax
+	mov [os_QueueLock], ax	; Release the lock
+	mov rax, r15
+	mov rbx, r12
+	mov rcx, r11
+	mov rsi, r10
+	mov rdi, r9
 	stc				; Carry set for failure (Queue full)
 	ret
 ; -----------------------------------------------------------------------------
@@ -157,50 +162,64 @@ os_smp_enqueue_fail:
 ; OUT:	RAX = Address of code to execute (Set to 0 if queue is empty)
 ;	RDI = Variable
 os_smp_dequeue:
-	push rsi
-	push rcx
+	mov  r9, rsi
+	mov r10, rcx
+	mov r11, rbx
+	mov r12, rdx
 
 os_smp_dequeue_spin:
-	bt word [os_QueueLock], 0	; Check if the mutex is free
-	jc os_smp_dequeue_spin		; If not check it again
-	lock bts word [os_QueueLock], 0	; The mutex was free, lock the bus. Try to grab the mutex
-	jc os_smp_dequeue_spin		; Jump if we were unsuccessful
-
-	cmp word [os_QueueLen], 0
-	je os_smp_dequeue_fail
+;	bt word [os_QueueLock], 0	; Check if the mutex is free
+;	jc os_smp_dequeue_spin		; If not check it again
+;	lock bts word [os_QueueLock], 0	; The mutex was free, lock the bus. Try to grab the mutex
+;	jc os_smp_dequeue_spin		; Jump if we were unsuccessful
 
 	xor ecx, ecx
+	mov cl, 1
+	xor eax, eax
+	lock cmpxchg cl, [os_QueueLock]
+	jz mutex_free
+	pause
+	jmp os_smp_dequeue_spin
+
+mutex_free:
 	mov rsi, cpuqueue
-	mov cx, [cpuqueuestart]
-	shl rcx, 4			; Quickly multiply RCX by 16
+	movzx ebx, word [os_QueueLen]
+	cmp eax, ebx
+	je os_smp_dequeue_fail
+
+	movzx ecx, word [cpuqueuestart]
+	mov edx, ecx
+	shl ecx, 4			; Quickly multiply RCX by 16
 	add rsi, rcx
 
-	lodsq				; Load the code address into RAX
-	push rax
-	lodsq				; Load the variable
-	mov rdi, rax
-	pop rax
+	mov rax, [rsi]				; Load the code address into RAX
+	mov rdi, [rsi+8]			; Load the variable
 
-	sub word [os_QueueLen], 1
-	shr rcx, 4			; Quickly divide RCX by 16
-	add cx, 1
-	cmp cx, [cpuqueuemax]
-	jne os_smp_dequeue_end
-	xor cx, cx			; We wrap around
+	dec ebx
+	xor r8d, r8d
+	inc edx
+	cmp edx, [cpuqueuemax]
+	cmove edx, r8d			; We wrap around	
 
 os_smp_dequeue_end:
-	mov word [cpuqueuestart], cx
-	pop rcx
-	pop rsi
-	btr word [os_QueueLock], 0	; Release the lock
+	mov word [cpuqueuestart], dx
+	xor edx, edx
+	mov [os_QueueLock], dx
+	mov rcx, r10
+	mov rsi, r9
+	mov r11, rbx
+	mov r12, rdx
 	clc				; If we got here then ok
 	ret
 
 os_smp_dequeue_fail:
-	xor rax, rax
-	pop rcx
-	pop rsi
-	btr word [os_QueueLock], 0	; Release the lock
+	xor eax, eax
+	mov [os_QueueLock], ax
+	mov rcx, r10
+        mov rsi, r9
+        mov r11, rbx
+        mov r12, rdx
+
 	stc
 	ret
 ; -----------------------------------------------------------------------------
@@ -221,8 +240,7 @@ os_smp_run:
 ;  IN:	Nothing
 ; OUT:	RAX = number of items in processing queue
 os_smp_queuelen:
-	xor eax, eax
-	mov ax, [os_QueueLen]
+	movzx eax, word [os_QueueLen]
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -232,8 +250,7 @@ os_smp_queuelen:
 ;  IN:	Nothing
 ; OUT:	RAX = number of cores in this computer
 os_smp_numcores:
-	xor eax, eax
-	mov ax, [os_NumCores]
+	movzx eax, word [os_NumCores]
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -248,27 +265,26 @@ os_smp_wait:
 	push rbx
 	push rax
 
-	call os_smp_get_id
-	mov rbx, rax
-
+	mov r9, [os_LocalAPICAddress] ; get_id
+        mov ebx, [r9+0x23]
 	xor eax, eax
-	xor ecx, ecx
+	xor edx, edx
+	mov ecx, 256
 	mov rsi, cpustatus
 
 checkit:
-	lodsb
+	movzx eax, byte [rsi]
 	cmp rbx, rcx		; Check to see if it is looking at itself
-	je skipit		; If so then skip as it shouild be marked as busy
-	bt ax, 0		; Check the Present bit
-	jnc skipit		; If carry is not set then the CPU does not exist
-	bt ax, 1		; Check the Ready/Busy bit
-	jnc skipit		; If carry is not set then the CPU is Ready
-	sub rsi, 1
-	jmp checkit		; Core is marked as Busy, check it again
-skipit:
-	add rcx, 1
-	cmp rcx, 256
-	jne checkit
+	sete dl
+				; Check the Present bit (bit 0)
+				; Check the Ready/Busy bit (bit 1)
+	test eax, 3
+	setnz al 
+	lea r8, [rsi+1]
+	or dl, al
+	cmovz rsi, r8
+	dec rcx
+	jnz checkit
 
 	pop rax
 	pop rbx
@@ -283,10 +299,23 @@ skipit:
 ;  IN:	RAX = Address of lock variable
 ; OUT:	Nothing. All registers preserved.
 os_smp_lock:
-	bt word [rax], 0	; Check if the mutex is free (Bit 0 cleared to 0)
-	jc os_smp_lock		; If not check it again
-	lock bts word [rax], 0	; The mutex was free, lock the bus. Try to grab the mutex
-	jc os_smp_lock		; Jump if we were unsuccessful
+	mov r8, rdi
+	mov r9, rcx
+	mov rdi, rax
+	xor ecx, ecx
+	mov cl, 1
+	xor eax, eax
+	lock cmpxchg [rdi], cl
+	jz mutex_free
+	pause
+	jmp os_smp_lock
+
+mutex_free:
+	mov rax, rdi
+	mov rdi, r8
+	mov rcx, r9
+
+	setc
 	ret			; Lock acquired. Return to the caller
 ; -----------------------------------------------------------------------------
 
@@ -296,7 +325,10 @@ os_smp_lock:
 ;  IN:	RAX = Address of lock variable
 ; OUT:	Nothing. All registers preserved.
 os_smp_unlock:
-	btr word [rax], 0	; Release the lock (Bit 0 cleared to 0)
+	mov r8, rbx
+	xor ebx, ebx
+	mov [rax], bx	; Release the lock (Bit 0 cleared to 0)
+	mov rbx, r8
 	ret			; Lock released. Return to the caller
 ; -----------------------------------------------------------------------------
 
